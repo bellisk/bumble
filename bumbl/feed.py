384e474b73533d3e081dd0021ae38c181d187c28
Rae Knowler <rae.knowler@liip.ch>
2024-10-29 18:03:21 +0100

    Delete everything and create new project

--- bumbl/feed.py
+++
@@ -1,7 +0,0 @@
-from django.utils.feedgenerator import Atom1Feed
-
-class BumbleFeed(Atom1Feed):
-    def add_item_elements(self, handler, item):
-        super().add_item_elements(handler, item)
-        if item['content'] is not None:
-            handler.addQuickElement("content", item['content'], {"type": "html"})
